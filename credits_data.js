fefbac46dbeb17386c9c48d98a9f7f1c242456ea
rgnsdev <rgnsdev@gmail.com>
2025-12-01 22:51:41 +0100

    Add files via upload

---
+++ credits_data.js
@@ -0,0 +1,48 @@
+const creditsData = `
+CUSTOM ART:
+
+Enemy Sprites:
+Romer Charles Benabaye (Charles)
+
+Backgrounds and Monster Platform:
+Baran B. Özden (Alenthas)
+
+PRE-MADE VISUAL ASSETS:
+
+Fonts:
+Raster Forge: GGBotNet - https://www.ggbot.net/
+Retro Gaming: Daymarius - https://www.dafont.com/profile.php?user=1002696
+
+Character Sprites:
+Fairy, Luce, Rosanne, Julie: BKX1 - https://bkx1.itch.io/super-combat-rpg-43
+
+Attack VFX:
+Viktor - https://v-ktor.itch.io/
+
+Gold and Item Category Icons:
+Dusk Games - https://dusk-games.itch.io/
+
+AUDIO:
+
+Custom Music:
+Suikale
+
+Pre-made Music:
+Boss Fight Music: kbrecordzz - https://freesound.org/people/kbrecordzz/
+
+Sound Effects - Combat:
+Attack Sound: wesleyextreme_gamer - https://freesound.org/people/wesleyextreme_gamer/
+Crit Sound Effect: saangosu - https://freesound.org/people/saangosu/
+Boss Appearance Sound: supersnd - https://freesound.org/people/supersnd/
+
+Sound Effects - Gameplay:
+Coin Found Sound: Mateusz_Chenc - https://freesound.org/people/Mateusz_Chenc/
+Item Drop Sound: IENBA - https://freesound.org/people/IENBA/
+Game Start Sound: plasterbrain - https://freesound.org/people/plasterbrain/
+Button Click Sound: Leszek Szary - https://freesound.org/people/Leszek_Szary/
+
+Sound Effects - Ambient & Other:
+Speech Sound: 666HeroHero - https://herohero.carrd.co/
+Wind Gust Sound: crashoverride6 - https://freesound.org/people/crashoverride6/
+Bell Sound: Slina - https://freesound.org/people/Slina/
+`;
